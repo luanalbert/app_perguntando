476ff204ff114e4cbc6d77ad771eec4e79ee0193
luanalbert <contatoluanalbert@gmail.com>
2021-02-13 18:51:50 -0300

    EJS entrando em açao

--- index.js
+++ index.js
@@ -6,11 +6,22 @@ app.set('view engine', 'ejs');//render de html - setado
 app.get("/:nome/:lang",(req,res) => { //:parametros obrigatorios
     var nome = req.params.nome; 
     var lang = req.params.lang;
+    var exibirMsg = false;
+
+    var produtos= [
+        {nome:"Doritos",preco: 3.50},
+        {nome:"Coca Cola",preco: 5.80},
+        {nome:"Red Bull",preco: 6.50},
+        {nome:"Cucuz",preco: 2.50},
+        {nome:"Suco",preco: 7.50}
+    ]
         res.render("index",{
         nome: nome,
         lang:lang,
         empresa: "Guia do porgramador",
-        inscritos: 1000
+        inscritos: 1000,
+        msg:exibirMsg,
+        produtos: produtos
     }); 
 });
 app.listen(8080,()=>{
