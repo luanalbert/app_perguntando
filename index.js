af121fb64e105b82f3200f8259c0b0a4acabeb43
luanalbert <contatoluanalbert@gmail.com>
2021-02-17 21:44:46 -0300

    tudo ok pra entrar no DB

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 app.set('view engine', 'ejs');//render de html - setado
+app.use(express.static('public'));//arquivos staticos
 
-app.get("/:nome/:lang",(req,res) => { //:parametros obrigatorios
-    var nome = req.params.nome; 
-    var lang = req.params.lang;
-    var exibirMsg = false;
+//bodyparser
+app.use(bodyParser.urlencoded({extended: false}));//traduzir os dados para que o js leia
+app.use(bodyParser.json());
 
-    var produtos= [
-        {nome:"Doritos",preco: 3.50},
-        {nome:"Coca Cola",preco: 5.80},
-        {nome:"Red Bull",preco: 6.50},
-        {nome:"Cucuz",preco: 2.50},
-        {nome:"Suco",preco: 7.50}
-    ]
-        res.render("index",{
-        nome: nome,
-        lang:lang,
-        empresa: "Guia do porgramador",
-        inscritos: 1000,
-        msg:exibirMsg,
-        produtos: produtos
-    }); 
+app.get("/",(req,res) => { //:parametros obrigatorios
+    res.render("index");
 });
+app.get("/perguntar",(req,res) => {
+    res.render("perguntar");
+});
+app.post("/salvarpergunta",(req,res) => {//recebendo dados do formulario
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+
+    res.send('formulario recebido' +titulo +" , "+ descricao);
+});
+
+
+
+
+
 app.listen(8080,()=>{
     console.log("App rodando!");
 });
